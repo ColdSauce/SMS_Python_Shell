c70afab2bba7e5529f7b98a64baa09f646ce6ded
Stefan <soccerfanatic1996@gmail.com>
2014-12-21 21:03:16 -0500

    added the functioning code. need to make much cleaner

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
-from twilio.rest import TwiloRestClient
+from flask import Flask, request, redirect
+import twilio.twiml
 app = Flask(__name__)
+
 
 @app.route('/',methods = ['GET','POST'])
 def root():
-
+	resp = twilio.twiml.Response()
+	evaluated = None
+	try:
+		evaluated = eval(str(request.form.get('Body')))
+	except:
+		exec(str(request.form.get('Body')))
+	if evaluated is not None:
+		resp.message(">> " + str(evaluated))
+	else:
+		resp.message(">> Your statement has been interpreted correctly.")
+	return str(resp)
 if __name__ == '__main__':
     app.run(debug = True)
